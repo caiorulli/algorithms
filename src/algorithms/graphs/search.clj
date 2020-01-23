4bea4f504068577f95302faf4aef95654cee8470
Caio Rulli <caiorulli@gmail.com>
2020-01-22 23:24:08 -0300

    Added bfs and dfs with distance

--- src/algorithms/graphs/search.clj
+++ src/algorithms/graphs/search.clj
@@ -16,6 +16,27 @@
                          (get graph u))
                  (conj visited u)))))))
 
+(defn bfs+d [graph source]
+  (loop [search   []
+         queue    [source]
+         visited  #{}
+         queued   #{source}
+         distance [0]]
+    (if (empty? queue)
+      {:elements search
+       :distance distance}
+      (let [u (first queue)
+            v (->> (get graph u)
+                   (filter (comp not queued))
+                   (filter (comp not visited)))]
+        (recur (conj search u)
+               (concat (rest queue) v)
+               (conj visited u)
+               (set (concat queued v))
+               (concat distance (repeat (count v)
+                                        (inc (last distance)))))))))
+
+
 (defn dfs [graph source]
   (loop [search  [source]
          stack   (get graph source)
@@ -31,3 +52,23 @@
                  (concat (get graph u)
                          (rest stack))
                  (conj visited u)))))))
+
+(defn dfs+d [graph source]
+  (loop [search   []
+         stack    [source]
+         visited  #{}
+         stacked  #{source}
+         distance [0]]
+    (if (empty? stack)
+      {:elements search
+       :distance distance}
+      (let [u (first stack)
+            v (->> (get graph u)
+                   (filter (comp not stacked))
+                   (filter (comp not visited)))]
+        (recur (conj search u)
+               (concat v (rest stack))
+               (conj visited u)
+               (set (concat stacked v))
+               (concat distance (repeat (count v)
+                                        (inc (last distance)))))))))
--- test/algorithms/graphs/search_test.clj
+++ test/algorithms/graphs/search_test.clj
@@ -1,5 +1,8 @@
 (ns algorithms.graphs.search-test
-  (:require [algorithms.graphs.search :refer [bfs dfs]]
+  (:require [algorithms.graphs.search :refer [bfs
+                                              bfs+d
+                                              dfs
+                                              dfs+d]]
             [midje.sweet :refer [=> fact]]))
 
 (fact "Breath first search"
@@ -10,6 +13,17 @@
             1 [0 2]
             2 [0 1]} 0) => [0 1 2])
 
+(fact "Breath first search with distance"
+      (bfs+d {0 [1 2 3]
+              2 [4]} 0) => {:elements [0 1 2 3 4]
+                            :distance [0 1 1 1 2]}
+      (bfs+d {0 [1 2]} 0) => {:elements [0 1 2]
+                              :distance [0 1 1]}
+      (bfs+d {0 [1 2]
+              1 [0 2]
+              2 [0 1]} 0) => {:elements [0 1 2]
+                              :distance [0 1 1]})
+
 (fact "Depth first search"
       (dfs {0 [1 2 3]
             2 [4]} 0) => [0 1 2 4 3]
@@ -17,3 +31,14 @@
       (dfs {0 [1 2]
             1 [0 2]
             2 [0 1]} 0) => [0 1 2])
+
+(fact "Depth first search with distance"
+      (dfs+d {0 [1 2 3]
+              2 [4]} 0) => {:elements [0 1 2 4 3]
+                            :distance [0 1 1 1 2]}
+      (dfs+d {0 [1 2]} 0) => {:elements [0 1 2]
+                              :distance [0 1 1]}
+      (dfs+d {0 [1 2]
+              1 [0 2]
+              2 [0 1]} 0) => {:elements [0 1 2]
+                              :distance [0 1 1]})
