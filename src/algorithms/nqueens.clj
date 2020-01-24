455a1a730811da2113b7bd7eb8618e29ec9520e9
Caio Rulli <caiorulli@gmail.com>
2020-01-23 23:19:32 -0300

    Finished nqueens (all combinations)

--- src/algorithms/nqueens.clj
+++ src/algorithms/nqueens.clj
@@ -1,4 +1,21 @@
 (ns algorithms.nqueens)
 
-(defn nqueens [n]
-  [])
+(defn nqueens
+  ([n]
+   (let [board []]
+     (nqueens n 0 board)))
+
+  ([n level board]
+   (if (= level n)
+     1
+     (reduce +
+             (for [i (range n)]
+               (if (some
+                    (fn [[j queen]]
+                      (contains? #{i (+ i (inc j)) (- i (inc j))}
+                                 queen))
+                    (zipmap (range level) board))
+                 0
+                 (nqueens n
+                          (inc level)
+                          (cons i board))))))))
--- test/algorithms/nqueens_test.clj
+++ test/algorithms/nqueens_test.clj
@@ -1,7 +1,15 @@
 (ns algorithms.nqueens-test
-  (:require [midje.sweet :refer [future-fact =>]]
+  (:require [midje.sweet :refer [fact =>]]
             [algorithms.nqueens :refer [nqueens]]))
 
-(future-fact "N-Queens"
-             (nqueens 4) => [[2 4 1 3]
-                             [3 1 4 2]])
+(fact "N-Queens"
+      (nqueens 0) => 1
+      (nqueens 1) => 1
+      (nqueens 2) => 0
+      (nqueens 3) => 0
+      (nqueens 4) => 2
+      (nqueens 5) => 10
+      (nqueens 6) => 4
+      (nqueens 7) => 40
+      (nqueens 8) => 92
+      (nqueens 9) => 352)
