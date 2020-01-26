dc3a61e7022f3029f23089a22c7388e8072d736c
Caio Rulli <caiorulli@gmail.com>
2020-01-26 00:07:46 -0300

    Removed horizontal simmetries (did it work?)

--- src/algorithms/nqueens.clj
+++ src/algorithms/nqueens.clj
@@ -1,6 +1,18 @@
-(ns algorithms.nqueens)
+(ns algorithms.nqueens
+  "N-Queens problem (https://en.wikipedia.org/wiki/Eight_queens_puzzle)")
+
+(defn- invalid-position?
+  "Checks for queens in conflict elsewhere in the board,
+  vertically or diagonally."
+  [i level board]
+  (some
+   (fn [[j queen]]
+     (contains? #{i (+ i (inc j)) (- i (inc j))}
+                queen))
+   (zipmap (range level) board)))
 
 (defn nqueens
+  "First attempt, all combinations."
   ([n]
    (let [board []]
      (nqueens n 0 board)))
@@ -10,11 +22,26 @@
      1
      (reduce +
              (for [i (range n)]
-               (if (some
-                    (fn [[j queen]]
-                      (contains? #{i (+ i (inc j)) (- i (inc j))}
-                                 queen))
-                    (zipmap (range level) board))
+               (if (invalid-position? i level board)
+                 0
+                 (nqueens n
+                          (inc level)
+                          (cons i board))))))))
+
+(defn nqueens+h
+  "Removing horizontal simmetries"
+  ([n]
+   (let [board []]
+     (nqueens n 0 board)))
+
+  ([n level board]
+   (if (= level n)
+     1
+     (reduce +
+             (for [i (range n)]
+               (if (or (and (= level 0)
+                            (> i (/ n 2)))
+                       (invalid-position? i level board))
                  0
                  (nqueens n
                           (inc level)
--- test/algorithms/nqueens_test.clj
+++ test/algorithms/nqueens_test.clj
@@ -1,6 +1,7 @@
 (ns algorithms.nqueens-test
   (:require [midje.sweet :refer [fact =>]]
-            [algorithms.nqueens :refer [nqueens]]))
+            [algorithms.nqueens :refer [nqueens
+                                        nqueens+h]]))
 
 (fact "N-Queens"
       (nqueens 0) => 1
@@ -13,3 +14,15 @@
       (nqueens 7) => 40
       (nqueens 8) => 92
       (nqueens 9) => 352)
+
+(fact "N-Queens, no horizontal simmetries"
+      (nqueens+h 0) => 1
+      (nqueens+h 1) => 1
+      (nqueens+h 2) => 0
+      (nqueens+h 3) => 0
+      (nqueens+h 4) => 2
+      (nqueens+h 5) => 10
+      (nqueens+h 6) => 4
+      (nqueens+h 7) => 40
+      (nqueens+h 8) => 92
+      (nqueens+h 9) => 352)
